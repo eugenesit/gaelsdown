3c63a67c9f959fd2761088a19f26824b0ee46f87
Eugene Sit <eugene.sit@queensu.ca>
2021-06-06 11:25:15 -0600

    need this for project?

---
+++ R/skeleton.R
@@ -0,0 +1,18 @@
+# from https://github.com/rstudio/bookdown/blob/master/R/utils.R
+gaelsown_file <- function(...) {
+  system.file(..., package = 'gaelsdown', mustWork = TRUE)
+}
+
+# from https://github.com/rstudio/bookdown/blob/master/R/skeleton.R
+gaelsdown_skeleton <- function(path = getwd()) {
+
+  # ensure directory exists
+  #  dir.create(path, recursive = TRUE, showWarnings = FALSE)
+
+  # copy 'resources' folder to path
+  resources <- gaelsdown_file('rstudio', 'templates', 'project', 'resources')
+
+  R.utils::copyDirectory(from = resources,
+                         to = path,
+                         recursive = TRUE)
+}
